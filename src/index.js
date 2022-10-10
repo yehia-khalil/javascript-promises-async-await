694018b64c48656eb81bf7ef8cc39cc88ffd5527
Yehia Khalil <yehiakhalilykk@gmail.com>
2022-10-10 21:42:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const movies = require('./data/movies.json');
-const fetchWithTimeout = require('./services')
+const { fetchWithTimeout } = require('./services')
 export function fetchMovies() {
     const resolveFunction = () => movies;
     return fetchWithTimeout(1000).then(resolveFunction)
