081562d5ffb82570bb9e188581da3bae38a44d52
Yehia Khalil <yehiakhalilykk@gmail.com>
2022-10-10 21:42:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ export function fetchMovies() {
     const resolveFunction = () => movies;
     return fetchWithTimeout(1000).then(resolveFunction)
 }
-let moviePromise = fetchMovies().then(results => console.log(results));
+let moviePromise = fetchMovies();
+moviePromise.then(results => console.log(results));
